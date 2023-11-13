366367fe8ed2b71118897c2b714b70ef16e2fddb
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-11-13 18:36:42 +0300

    tailwind configuration and setup

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function Home() {
+  return (
+    <div className='text-cyan-800 text-center'>
+      my name is lincoln
+    </div>
+  )
+}
+
+export default Home

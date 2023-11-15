bfb8788d905c5e41b7bd89377d865a2af2544327
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-11-15 21:55:29 +0300

    align code for easiar readibility

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Footer() {
   return (
     <footer className="bg-gray-900 text-white">
-    <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
-      <h1>So How Does It Work</h1>
-      <p>Just follow this simple steps</p>
-   </div>
+        <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
+            <h1>So How Does It Work</h1>
+            <p>Just follow this simple steps</p>
+        </div>
 
   </footer>>
   )

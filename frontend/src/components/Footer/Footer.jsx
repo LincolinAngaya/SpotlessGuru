17931f42eb30aba64da08fef4881adb19e030865
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-11-15 21:52:48 +0300

    add new heading 1 to the fooetr section

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Footer() {
   return (
     <footer className="bg-gray-900 text-white">
-  <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
-
+    <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
+      <h1>So How Does It Work</h1>
    </div>
 
   </footer>>

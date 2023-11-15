6def75bde688e779ffc703013b8f1b30c515cde0
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-11-15 21:53:48 +0300

    add aparagraph to the section

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -5,6 +5,7 @@ function Footer() {
     <footer className="bg-gray-900 text-white">
     <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
       <h1>So How Does It Work</h1>
+      <p>Just follow this simple steps</p>
    </div>
 
   </footer>>

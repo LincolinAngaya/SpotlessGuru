5da39b0da512a3d44d6be54ad9051bc035adebac
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-11-15 22:08:19 +0300

    done adding steps to follow

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
-
+import ItemsContainer
 function Footer() {
   return (
     <footer className="bg-gray-900 text-white">
         <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
             <h1>So How Does It Work</h1>
             <p>Just follow this simple steps</p>
+        </div>
+        <div>
+            <div>
+             <h1>You Order</h1>
+             <p>Create your account or log in and place order</p>
+            </div>
+
+            <div>
+            <h1>We Come</h1>
+            <p>we send clreaner based on your location and service</p>
+           </div>
+
+           <div>
+           <h1>We Clean</h1>
+           <p>We proffessionally clean you space based on your preference</p>
+          </div>
+           
+          <div>
+           <h1>You Smile</h1>
+           <p>we make you smile with Satisfaction</p>
+          </div>
+          <ItemsContainer />
         </div>
 
   </footer>>

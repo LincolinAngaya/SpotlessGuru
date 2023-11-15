c09f9458fb8fa926affce7362fb93f52ddeea50a
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-11-15 21:50:26 +0300

    create components inheriting each other for easier development of footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-      
-    </div>
+    <footer className="bg-gray-900 text-white">
+  <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
+
+   </div>
+
+  </footer>>
   )
 }
 

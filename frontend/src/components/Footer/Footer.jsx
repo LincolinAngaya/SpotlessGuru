8b84309e03ef5b817259254785d0b65a7bd44c91
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-11-16 22:08:46 +0300

    add new component

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -5,39 +5,14 @@ import { Icons } from "../menu";
 
 function Footer() {
   return (
-    <footer className="bg-gray-900 text-white">
-        <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
-            <h1>So How Does It Work</h1>
-            <p>Just follow this simple steps</p>
-        </div>
-        <div className="bg-custom-navy">
-        <div>
-            <div>
-             <h1>You Order</h1>
-             <p>Create your account or log in and place order</p>
-            </div>
-
-            <div>
-            <h1>We Come</h1>
-            <p>we send clreaner based on your location and service</p>
-           </div>
-
-           <div>
-           <h1>We Clean</h1>
-           <p>We proffessionally clean you space based on your preference</p>
-          </div>
-           
-          <div>
-           <h1>You Smile</h1>
-           <p>we make you smile with Satisfaction</p>
-          </div>
-         
-        </div>
-
-        <ItemsContainer />
+    
+    <footer className="text-white">
+        
+        <div className="bg-custom-navy text-white">
+               <ItemsContainer />
  
-        <div className="mx-auto p-8 mt-16 bg-custom-navy text-white text-center relative">
-        <div className="border-t border-gray-400 absolute left-1/2 transform -translate-x-1/2 w-4/5"></div>
+        <div className="bg-custom-navy text-white text-center relative">
+         <div className="border-t-2 border-white absolute left-1/2 transform -translate-x-1/2 w-3/4"></div>
         <SocialIcons Icons={Icons} />
         <span>Spotless Guru Cleaners</span>
         <div className="mt-4">Copyright &copy; {new Date().getFullYear()} Spotless Guru Cleaners</div>

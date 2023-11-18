acdd79079da55046967047c955397ded114a2370
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-11-18 16:28:00 +0300

    done styling my branch

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -19,7 +19,7 @@ function Footer() {
             <SocialIcons Icons={Icons} />
           </div>
         </div>
-        <span className="block mb-4 text-lg">Spotless Guru Cleaners</span>
+        <span className="block text-lg">Spotless Guru Cleaners</span>
         <div className="mt-4 bg-navy text-white text-center p-4">
           Copyright &copy; {new Date().getFullYear()} Spotless Guru Cleaners
         </div>
--- frontend/src/components/Footer/SocialIcons.jsx
+++ frontend/src/components/Footer/SocialIcons.jsx
@@ -10,9 +10,9 @@ const SocialIcons = ({ Icons }) => {
           href={icon.link}  // Add a link property to each icon object
           target="_blank"
           rel="noopener noreferrer"
-          className="p-2 cursor-pointer inline-flex items-center
-          rounded-full mx-3.5 text-xl hover:text-white hover:hover:text-sky
-          duration-300 ">
+          className="p-3  cursor-pointer inline-flex items-center
+          rounded-full mx-2.5 text-3xl hover:text-white hover:hover:text-sky
+          duration-300">
           <ion-icon name={icon.name}></ion-icon>
         </span>
       ))}

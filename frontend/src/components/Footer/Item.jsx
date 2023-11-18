cc6b6d6bcf581e5773e6cad5d89dbd9fa9d23e65
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-11-18 10:43:33 +0300

    add email tag in order for user to send emails gto our apploication

--- frontend/src/components/Footer/Item.jsx
+++ frontend/src/components/Footer/Item.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 
 
-const Item = ({ Links, title }) => {
+const Item = ({ Links, title, }) => {
   return (
     <ul>
       <h1 className="mb-1 font-semibold">{title}</h1>
       {Links.map((link) => (
         <li key={link.name}>
+  
         <a
   className="text-white hover:text-sky-400 duration-300 font-sans
-          text-sm cursor-pointer leading-6 ml-4 capitalize"
+          text-sm cursor-pointer "
   href={link.link}
 >
-{link.icon} <span className="mr-2"></span> {link.name}
+  {link.name}
           
           </a>
-          <div></div>
+          
         </li>
       ))}
     </ul>

419fcca8c8db842c60c03b77259fdf41ff7f7910
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-11-18 12:12:06 +0300

    add icons to components

--- frontend/src/components/Footer/BookingSteps.jsx
+++ frontend/src/components/Footer/BookingSteps.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { MdShoppingCart } from "react-icons/md";
-import { AiOutlineCheckCircle } from "react-icons/ai";
+import order from '../assets/order.png'
+import come from '../assets/come.png'
+import clean from '../assets/clean.png'
+import happy from '../assets/happy.png'
 
 
 function BookingSteps() {
@@ -15,25 +17,27 @@ function BookingSteps() {
 
 <div>
     <div>
-    <MdShoppingCart size={30} /> <AiOutlineCheckCircle  />
+    <img src={order} alt="order now" />
      <h1>You Order</h1>
      <p>Create your account or log in and place order</p>
     </div>
 
     <div>
-    
+    <img src={come} alt="come now" />
     <h1>We Come</h1>
     <p>we send clreaner based on your location and service</p>
    </div>
 
    <div>
+   <img src={clean} alt="clean now" />
    <h1>We Clean</h1>
    <p>We proffessionally clean you space based on your preference</p>
   </div>
    
   <div>
+  <img src={happy} alt="happy now" />
    <h1>You Smile</h1>
-   <p>we make you smile with Satisfaction</p>
+   <p>we make you smile with Satisfaction and give us feedback</p>
   </div>
  
 </div>

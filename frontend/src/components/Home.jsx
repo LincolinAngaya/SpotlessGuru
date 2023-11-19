ef3d1e1bdb3abb8484cc7937496671905f103423
NobertSwaka <swakanobert@gmail.com>
2023-11-19 20:23:18 +0300

    Navigation bar

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,3 +1,16 @@
-import React from 'react';
+import React from 'react'
+import Footer from './Footer/Footer'
+import Navbar from "./NavBar/Navbar.1"
+function Home() {
+    return (
+        <>
+            <div>
+                <Navbar />
+                <Footer />
 
+            </div>
+        </>
+    )
+}
 
+export default Home
--- frontend/src/components/NavBar/Navbar.1.jsx
+++ frontend/src/components/NavBar/Navbar.1.jsx
@@ -1,13 +1,11 @@
-import "../NavBar/styles.css"
-import logo from "../assets/guru.png";
 export default function Navbar() {
     return <>
-        <div className="nav_1"> {/*Call: 0796286466*/}</div>
+        <div className="nav_1"> Call: 0796286466</div>
 
         <nav className="nav">
-            <div className="logo">
-                <img src={logo} alt="Logo" width="100" height="25" />
-            </div>
+            <a href="#" class="header-logo">
+                <img src="../assets/guru.png" alt="logo" width="300" height="100">
+                </></a>
             <ul>
                 <li className="active">
                     <a href="/home">Home</a>
@@ -24,8 +22,10 @@ export default function Navbar() {
                 <li>
                     <a href="/contact">Contact Us</a>
                 </li>
-
+                <li>
+                    <a href="/cart">Cart</a>
+                </li>
             </ul>
         </nav>
-    </>
+    </>;
 }

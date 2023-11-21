a4fa5dd5f56a8abe24ccd281936c5b0fe6ecd445
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-11-21 22:22:27 +0300

    corrected the errors

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/Spotless guru black.svg';
 
-
 const Navbar = () => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
   };
 
   return (
@@ -18,83 +21,90 @@ const Navbar = () => {
         </a>
       </div>
 
-      <div className="container mx-auto flex justify-around">
-        <Link to="/" className="text-customBlue text-lg font-signature font-bold">
-          <img src={logo} alt="Logo" className="h-16" />
-        </Link>
-
-        {/* Responsive Menu Icon */}
-        <div className="md:hidden">
-          <button onClick={toggleDropdown} className="text-white focus:outline-none">
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-            </svg>
-          </button>
-        </div>
-
-        {/* Regular Navigation */}
-        <ul className="md:flex gap-5 md:w-2/3">
-          <li className='ml-7 p-3'>
-            <Link to="/" className="text-customBlue hover:bg-sky text-lg font-signature font-bold">
-              Home
-            </Link>
-          </li>
-          <li className='ml-7 p-3'>
-            <Link to="/about" className="text-customBlue hover:bg-sky text-lg font-signature font-bold">
-              About Us
-            </Link>
-          </li>
-          <li className="md:relative ml-7 p-3">
-            {/* Services link with dropdown */}
-            <button
-              onClick={toggleDropdown}
-              className="text-customBlue hover:bg-sky text-lg font-signature font-bold focus:outline-none"
-            >
-              Services
-            </button>
-            {isDropdownOpen && (
-              <div className="absolute top-full left-0 mt-2 bg-white rounded shadow-md md:hidden">
-                <ul>
-                  <li>
-                    <Link to="/services/adish-washing" className="block px-4 py-2 text-customBlue hover:bg-gray-200">
-                      Adish Washing
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/services/housecleaning" className="block px-4 py-2 text-customBlue hover:bg-gray-200">
-                      Housecleaning
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/services/clothes-hand-washing"
-                      className="block px-4 py-2 text-customBlue hover:bg-gray-200"
-                    >
-                      Clothes Hand Washing
-                    </Link>
-                  </li>
-                </ul>
-              </div>
+      <header className={`bg-white ${isMenuOpen ? 'md:min-h-fit min-h-[60vh]' : ''}`}>
+        <nav className="nav flex justify-between items-center">
+          <div className="ml-4 p-3">
+            {/* Hide logo on smaller screens */}
+            {!isMenuOpen && (
+              <Link to="/" className="text-customBlue text-lg font-signature font-bold">
+                <img src={logo} alt="Logo" className="w-16 cursor-pointer" />
+              </Link>
             )}
-          </li>
-          <li className='ml-7 p-3'>
-            <Link to="/testimonials" className="text-customBlue hover:bg-sky text-lg font-signature font-bold">
-              Testimonials
-            </Link>
-          </li>
-          <li className='ml-7 p-3'>
-            <Link to="/contact" className="text-customBlue hover:bg-sky text-lg font-signature font-bold">
-              Contact Us
-            </Link>
-          </li>
-        </ul>
-      </div>
+          </div>
+
+          <div
+            className={`nav-links duration-500 md:static absolute ${
+              isMenuOpen ? 'bg-white top-[9%]' : 'md:min-h-fit min-h-[60vh] -top-full'
+            } md:w-auto w-full flex flex-col items-center md:flex-row md:gap-[4vw] gap-8 px-2`}
+          >
+            <ul className="flex flex-col items-center md:flex-row md:items-center w-full">
+              <li className="relative">
+                <Link
+                  onClick={closeMenu}
+                  to="/"
+                  className={`text-lg font-signature font-bold ${
+                    isMenuOpen ? 'text-customBlue' : 'text-customBlue hover:text-sky'
+                  }`}
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="ml-0 md:ml-7 p-3">
+                <Link
+                  onClick={closeMenu}
+                  to="/about"
+                  className={`text-lg font-signature font-bold ${
+                    isMenuOpen ? 'text-customBlue' : 'text-customBlue hover:text-sky'
+                  }`}
+                >
+                  About Us
+                </Link>
+              </li>
+              <li className="ml-0 md:ml-7 p-3">
+                <Link
+                  onClick={closeMenu}
+                  to="/services"
+                  className={`text-lg font-signature font-bold ${
+                    isMenuOpen ? 'text-customBlue' : 'text-customBlue hover:text-sky'
+                  }`}
+                >
+                  Services
+                </Link>
+              </li>
+              <li className="ml-0 md:ml-7 p-3">
+                <Link
+                  onClick={closeMenu}
+                  to="/testimonials"
+                  className={`text-lg font-signature font-bold ${
+                    isMenuOpen ? 'text-customBlue' : 'text-customBlue hover:text-sky'
+                  }`}
+                >
+                  Testimonials
+                </Link>
+              </li>
+              <li className="ml-0 md:ml-7 p-2"> {/* Adjusted the padding here */}
+                <Link
+                  onClick={closeMenu}
+                  to="/contact"
+                  className={`text-lg font-signature font-bold ${
+                    isMenuOpen ? 'text-customBlue' : 'text-customBlue hover:text-sky'
+                  }`}
+                >
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="flex items-center gap-6">
+            <ion-icon
+              onClick={toggleMenu}
+              name={isMenuOpen ? 'close' : 'menu'}
+              className="text-3xl cursor-pointer md:hidden"
+            ></ion-icon>
+          </div>
+        </nav>
+      </header>
     </div>
   );
 };

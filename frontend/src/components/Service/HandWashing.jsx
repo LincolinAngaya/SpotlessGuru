2ae25e3882bd9e2976816b68892da149ba7a8581
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-11-23 21:41:34 +0300

    done coding handwashing

---
+++ frontend/src/components/Service/HandWashing.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Footer from '../Footer/Footer'
+
+function HandWashing() {
+  return (
+    <div>
+      <div>Clothes Hand Washing</div>
+
+      <div>
+      <h1>Ready for affordable, hassle-free clothes hand washing services?</h1>
+      <p>Enjoy sparkling clean clothes every week with our friendly-priced recurring hand washing services. Say goodbye to laundry worries without breaking the bank!</p>
+      </div>
+
+      <div>
+      <h1>Our hand washing Includes</h1>
+      <div>
+      <div>
+      <img src='' alt='Handwashing' />
+      <ul>
+        <li>
+            <strong>Bedding:</strong>
+            Hand washing of bed linens, sheets, pillowcases, and duvet covers to ensure a fresh and clean sleeping environment.
+        </li>
+        <li>
+            <strong>Shoes:</strong>
+            Gentle cleaning of shoes to remove dirt and stains. This can include various types of shoes, from casual sneakers to delicate dress shoes.
+        </li>
+        <li>
+            <strong>Clothes:</strong>
+            Handwashing services for a wide range of clothing items, including everyday wear, delicates, wool garments, activewear, and special fabrics.
+        </li>
+     </ul>
+
+    <button type="button">Book Now</button>
+
+      </div>
+      
+      </div>
+      
+      </div>
+      <Footer />
+
+    </div>
+  )
+}
+
+export default HandWashing

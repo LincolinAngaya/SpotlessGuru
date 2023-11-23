fe818762c0f2ce7322b9ea1e4c23bbc6c92146cf
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-11-23 21:20:18 +0300

    modify components inheritance

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import HouseCleaning from './components/Service/HouseCleaning'
 import Home from './Home'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    
+  } from 'react-router-dom';
 
 function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  )
+    return (
+        <>
+  
+        <Router> 
+             <div>
+             <Navbar/>
+             <Routes>
+             <Route exact path='/' element={<Home />}></Route>
+              <Route exact path='/housecleaning' element={<HouseCleaning />}></Route>
+             </Routes>
+             
+             </div>
+        </Router>
+      
+
+       
+        </>
+    )
 }
 
 export default App
-
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import Footer from './components/Footer/Footer'
  import Slider2 from './components/Hero/Slider2'
  import BookingSteps from './components/Footer/BookingSteps'
-import Navbar from './components/Navbar/Navbar'
-import HouseCleaning from './components/Service/HouseCleaning'
 
 import {
     BrowserRouter as Router,
@@ -11,22 +9,14 @@ import {
     Route,
     
   } from 'react-router-dom';
-
 function Home() {
     return (
         <>
-  
-        <Router> 
-    
-           <Navbar/>
-         
-       </Router>
        <Slider2 />
        <BookingSteps />
-       
-      
        <Footer />
-       <HouseCleaning />
+
+       
         </>
     )
 }

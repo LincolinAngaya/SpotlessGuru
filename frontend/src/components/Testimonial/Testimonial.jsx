5cd810aed2f33160e3bf47cce5bf000729eca7c5
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-11-23 22:00:25 +0300

    done designing testimonial pages

---
+++ frontend/src/components/Testimonial/Testimonial.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Footer from '../Footer/Footer'
+
+function Testimonial() {
+  return (
+    <div>
+      <div>Testimonials</div>
+
+      <div>
+      <h1>At Spotless Guru Cleaners, our stellar ratings and reviews are earned through consistent delivery on promises. If we miss the mark, we make it right, always. Our skilled cleaning technicians bring shine and care, complemented by a friendly, customer-focused office staff. Your satisfaction is our priority!</h1>
+      
+      </div>
+
+
+      
+      <div>
+      <div>
+      <h2>Dan Baraka</h2>
+      <p>Qejani Residence-Karen</p>
+      <img src='' alt='image' />
+      <div>Spotless Guru, you're my savior! I was broke, but with just a coin in my pocket, I could afford your amazing dish washing services. Quality that fits any budget—thank you for making cleanliness accessible!</div>
+      </div>
+
+      <div>
+      <h2>Rachael Njoroge</h2>
+      <p>Elite Hostel-Karen</p>
+      <img src='' alt='image' />
+      <div>Spotless Guru transformed our hostel kitchen! With efficient dish washing services, they lifted the post-move-in mess, ensuring a spotless and stress-free communal kitchen. Spotless Guru, our hostel's cleaning superhero!</div>
+      </div>
+
+      <div>
+      <h2>Dan Kinyanjui</h2>
+      <p>SKL hostels-Gataka</p>
+      <img src='' alt='image' />
+      <div>Spotless Guru cleaners  is a lifesaver for hostel living! Their house cleaning services turned my chaotic space into a haven. I can focus on studies, not mess. Plus, they're affordable and respond immediately. Thank you, Spotless Guru!</div>
+      </div>
+
+      <div>
+      <h2>Eve Nyaboke</h2>
+      <p>Cooperative University-Karen</p>
+      <img src='' alt='image' />
+      <div>Spotless Guru's hand washing service is a game-changer! No more laundry stress in the hostel. Affordable, efficient, and my clothes have never looked better. After trying others that were expensive, I found a gem with Spotless Guru – cheaper and better than the rest!</div>
+      </div>
+      </div>
+      <Footer />
+
+    </div>
+  )
+}
+
+export default Testimonial

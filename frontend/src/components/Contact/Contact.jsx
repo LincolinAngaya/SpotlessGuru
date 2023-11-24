9c37d7e13d5e71adfd1d8a5ff7d3210b927e2e98
phinic925 <phinicnyangate480@gmail.com>
2023-11-24 12:03:28 +0300

    adds responsiveness

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -8,10 +8,10 @@ const Contact = () => {
         <h2 className='bg-gradient-to-b from-customBlue to-sky text-white p-6 text-center font-signature text-3xl'>Contact us</h2>
       </div>
       <div className='relative'>
-        <div className='container mx-auto mt-10 p-4 ml-16'>
+        <div className='container mx-auto mt-10 p-4 md:ml-16'>
           <div className='wrapper relative'>
             {/* Additional Container on Top */}
-            <div className='absolute top-0 right-0 bg-navy text-white p-20 mb-4 mr-36'>
+            <div className='absolute top-0 right-0 bg-navy text-white p-4 md:p-20 mb-4 md:mr-16'>
               <p>Info</p>
               <p> <AiOutlineMail />spotlessGuru@gmail.com</p>
               <p> <AiOutlinePhone />+25496286466</p>
@@ -22,37 +22,37 @@ const Contact = () => {
             <div>
               <h2 className='text-3xl font-bold mb-4'>Contact Us</h2>
               <form className='max-w-md'>
-              <div className='mb-4'>
-                <input
-                  type='text'
-                  id='name'
-                  name='name'
-                  placeholder='Enter your Name'
-                  className='mt-1 p-2 w-full border rounded-md'
-                  required
-                />
-              </div>
+                <div className='mb-4'>
+                  <input
+                    type='text'
+                    id='name'
+                    name='name'
+                    placeholder='Enter your Name'
+                    className='mt-1 p-2 w-full border rounded-md'
+                    required
+                  />
+                </div>
 
-              <div className='mb-4'>
-                <input
-                  type='email'
-                  id='email'
-                  name='email'
-                  placeholder='Enter your Email'
-                  className='mt-1 p-2 w-full border rounded-md'
-                  required
-                />
-              </div>
+                <div className='mb-4'>
+                  <input
+                    type='email'
+                    id='email'
+                    name='email'
+                    placeholder='Enter your Email'
+                    className='mt-1 p-2 w-full border rounded-md'
+                    required
+                  />
+                </div>
 
-              <div className='mb-4'>
-                <textarea
-                  id='message'
-                  name='message'
-                  placeholder='Enter your message'
-                  className='mt-1 p-2 w-full border rounded-md resize-none'
-                  required
-                ></textarea>
-              </div>
+                <div className='mb-4'>
+                  <textarea
+                    id='message'
+                    name='message'
+                    placeholder='Enter your message'
+                    className='mt-1 p-2 w-full border rounded-md resize-none'
+                    required
+                  ></textarea>
+                </div>
 
                 <button
                   type='submit'

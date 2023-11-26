c86f72408ce3123b3e7403f563516dd4352b1b50
phinic925 <phinicnyangate480@gmail.com>
2023-11-26 16:16:42 +0300

    made changes to the contact page

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -7,35 +7,10 @@ const Contact = () => {
     <>
       <h2 className='bg-gradient-to-r from-customBlue to-sky text-white p-6 text-center font-signature text-3xl mb-4'>Contact us</h2>
       <div className='container mx-auto shadow-md bg-white p-4'>
-        <div className="bg-gray-100 p-4">
-          <div className='relative'>
-            {/* Additional Container on Top */}
-            <div className='absolute top-0 right-0 bg-navy text-white p-4 md:p-20 mb-4 md:mr-16'>
-              <p className='text-center text-3xl'>Info</p>
-              <p className='flex items-center text-2xl'><AiOutlineMail className='mr-2' /> spotlessGuru@gmail.com</p>
-              <p className='flex items-center text-2xl'> <AiOutlinePhone className='mr-2' /> +25496286466</p>
-              <p className='flex items-center text-2xl'> <AiOutlineEnvironment className='mr-2'/> Karen, Nairobi</p>
-              <p className='flex items-center text-2xl'><AiOutlineClockCircle className='mr-2'/> 7:00 a.m - 6:00 p.m</p>
-
-              {/* Social Media Icons */}
-              <div className='flex items-center mt-4'>
-                <a href="#facebook" className='text-white mr-4' target="_blank" rel="noopener noreferrer">
-                  <FaFacebook size={24} />
-                </a>
-                <a href="#twitter" className='text-white mr-4' target="_blank" rel="noopener noreferrer">
-                  <FaTwitter size={24} />
-                </a>
-                <a href="#instagram" className='text-white mr-4' target="_blank" rel="noopener noreferrer">
-                  <FaInstagram size={24} />
-                </a>
-                <a href="#linkedin" className='text-white' target="_blank" rel="noopener noreferrer">
-                  <FaLinkedin size={24} />
-                </a>
-              </div>
-            </div>
-
-            {/* Form Container */}
-            <div>
+        <div className="bg-gray p-4">
+          <div className='flex flex-col md:flex-row gap-8 items-center'>
+             {/* Form Container */}
+            <div className='md:flex-1 mb-4 md:mb-0'>
               <h2 className='text-2xl font-bold mb-4'>Contact Us</h2>
               <form className='max-w-md'>
               <div className='mb-4'>
@@ -76,9 +51,50 @@ const Contact = () => {
             >
               Send
             </button>
-                {/* Form Input Fields */}
+                
               </form>
             </div>
+            <div className='bg-navy text-white flex-1 p-4 mt-4 rounded-md'>
+  <p className='text-center text-2xl font-bold mb-2'>Info</p>
+  <p className='flex items-center justify-center text-xl'>
+    <AiOutlineMail className='mr-2' /> spotlessGuru@gmail.com
+  </p>
+  <p className='flex items-center justify-center text-xl'>
+    <AiOutlinePhone className='mr-2' /> +25496286466
+  </p>
+  <p className='flex items-center justify-center text-xl'>
+    <AiOutlineEnvironment className='mr-2'/> Karen, Nairobi
+  </p>
+  <p className='flex items-center justify-center text-xl'>
+    <AiOutlineClockCircle className='mr-2'/> 7:00 a.m - 6:00 p.m
+  </p>
+
+  {/* Social Media Icons */}
+  <div className='flex items-center justify-center mt-4'>
+    <a href="#facebook" className='text-white mr-4 p-2 cursor-pointer inline-flex items-center
+      rounded-full mx-2.5 text-2xl hover:text-white hover:hover:text-sky
+      duration-300' target="_blank" rel="noopener noreferrer">
+      <FaFacebook size={20} />
+    </a>
+    <a href="#twitter" className='text-white mr-4 p-2 cursor-pointer inline-flex items-center
+      rounded-full mx-2.5 text-2xl hover:text-white hover:hover:text-sky
+      duration-300' target="_blank" rel="noopener noreferrer">
+      <FaTwitter size={20} />
+    </a>
+    <a href="#instagram" className='text-white mr-4 p-2 cursor-pointer inline-flex items-center
+      rounded-full mx-2.5 text-2xl hover:text-white hover:hover:text-sky
+      duration-300' target="_blank" rel="noopener noreferrer">
+      <FaInstagram size={20} />
+    </a>
+    <a href="#linkedin" className='text-white p-2 cursor-pointer inline-flex items-center
+      rounded-full mx-2.5 text-2xl hover:text-white hover:hover:text-sky
+      duration-300' target="_blank" rel="noopener noreferrer">
+      <FaLinkedin size={20} />
+    </a>
+  </div>
+</div>
+
+
           </div>
         </div>
       </div>
